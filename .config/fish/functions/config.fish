e84f8b374649aa45cf3e8e7156195f66f7335784
Jake Bailey <asaxplayinghorse@gmail.com>
2025-04-10 00:32:31 -0700

    Installed LazyVim

--- .config/fish/functions/config.fish
+++ .config/fish/functions/config.fish
@@ -1,4 +1,4 @@
-function config --wraps='/usr/bin/git --git-dir=/home/jacbaile/.cfg/ --work-tree=/home/jacbaile' --description 'alias config /usr/bin/git --git-dir=/home/jacbaile/.cfg/ --work-tree=/home/jacbaile'
+function config --wraps='/usr/bin/git --git-dir=/home/jacbaile/.cfg/ --work-tree=/home/jacbaile' --description 'Wrapper for my dotfiles git repo.'
   /usr/bin/git --git-dir=/home/jacbaile/.cfg/ --work-tree=/home/jacbaile $argv
         
 end
